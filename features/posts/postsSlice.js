d27eafa4ca2f77ed5498194106d32018cdec1d0b
Gideon Mba <abaaneg@gmail.com>
2023-05-15 21:00:41 +0000

    done posts and users

--- features/posts/postsSlice.js
+++ features/posts/postsSlice.js
@@ -1,41 +1,12 @@
 import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
-import { sub } from 'date-fns'
+import { sub } from 'date-fns';
 import axios from "axios";
 
-// const initialState = [
-//     {
-//         id: '1', 
-//         title: 'Learning Redux Toolkit', 
-//         content: "I've heard good things ", 
-//         date: sub(new Date(), { minutes: 10 }).toISOString(),
-//         reactions: {
-//             thumbsUp: 0,
-//             wow: 0,
-//             heart: 0,
-//             rocket: 0,
-//             coffee: 0
-//         }
-// },
-//     {
-//         id: '2',
-//         title: 'Slices...',
-//         content: "The more you reach the more i teach",
-//         date: sub(new Date(), { minutes: 5 }).toISOString(),
-//         reactions: {
-//             thumbsUp: 0,
-//             wow: 0,
-//             heart: 0,
-//             rocket: 0,
-//             coffee: 0
-//         }
-//     }
-// ]
-
-const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 const initialState = {
     posts: [],
-    status: 'idle', //'idle' | 'loading' | succeeded | failed
+    status: 'idle', //'idle' | 'loading' | 'succeeded' | 'failed'
     error: null
 }
 
@@ -49,8 +20,8 @@ export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPos
     return response.data
 })
 
-const postsSlice = createSlice ({
-    name: 'posts' ,
+const postsSlice = createSlice({
+    name: 'posts',
     initialState,
     reducers: {
         postAdded: {
@@ -91,10 +62,10 @@ const postsSlice = createSlice ({
             })
             .addCase(fetchPosts.fulfilled, (state, action) => {
                 state.status = 'succeeded'
-                //Adding date and reactions
+                // Adding date and reactions
                 let min = 1;
                 const loadedPosts = action.payload.map(post => {
-                    post.date = sub(new Date(), { minutes: min++}).toISOString()
+                    post.date = sub(new Date(), { minutes: min++ }).toISOString();
                     post.reactions = {
                         thumbsUp: 0,
                         wow: 0,
@@ -102,10 +73,10 @@ const postsSlice = createSlice ({
                         rocket: 0,
                         coffee: 0
                     }
-                    return post
-                })
+                    return post;
+                });
 
-                //Add any fetched posts to the arrray
+                // Add any fetched posts to the array
                 state.posts = state.posts.concat(loadedPosts)
             })
             .addCase(fetchPosts.rejected, (state, action) => {
@@ -129,10 +100,10 @@ const postsSlice = createSlice ({
                 action.payload.date = new Date().toISOString();
                 action.payload.reactions = {
                     thumbsUp: 0,
-                    wow: 0,
+                    hooray: 0,
                     heart: 0,
                     rocket: 0,
-                    coffee: 0
+                    eyes: 0
                 }
                 console.log(action.payload)
                 state.posts.push(action.payload)
@@ -140,9 +111,9 @@ const postsSlice = createSlice ({
     }
 })
 
-export const selectAllPosts = (state) => state.posts.posts
-export const getPostsStatus = (state) => state.posts.status
-export const getPostsError = (state) => state.posts.error
+export const selectAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
 
 export const { postAdded, reactionAdded } = postsSlice.actions
 
