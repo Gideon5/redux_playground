653f1133665f0eca7d0e1edc605d04dba506fe56
Gideon Mba <abaaneg@gmail.com>
2023-05-16 16:01:20 +0000

    crud done

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -5,11 +5,16 @@ import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 
+import { useParams, Link } from "react-router-dom";
+
 
 const SinglePostPage = () => {
     //retrieve postId
+    const { postId } = useParams()
 
-    const post = useSelector((state) => selectPostById(state, postId))
+    const post = useSelector((state) => selectPostById(state, Number(postId)))
+
+    // const post = useSelector((state) => selectPostById(state, postId))
 
     if (!post) {
         return (
@@ -24,6 +29,7 @@ const SinglePostPage = () => {
         <h2>{post.title}</h2>
         <p>{post.body}</p>
         <p>
+            <Link to={`/post/edit/${post.id}`}>Edit Post</Link>
             <PostAuthor userId={post.userId}/>
             <TimeAgo timestamp={post.date}/>
         </p>
